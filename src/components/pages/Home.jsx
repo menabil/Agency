3c31ecf5f26ad8eff1fe9b4785a0db5efdd6e74a
Nabil <nabil99cc@gmail.com>
2026-02-04 12:15:56 +0600

    feat(Home): add Home page

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+import Banner from "../layouts/Banner";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
